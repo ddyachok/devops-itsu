531e5bedc645622108cdbd613c8ca26691ffa95d
ddyachok <riwlay@gmail.com>
2021-12-08 18:36:00 +0200

    add hw3

---
+++ hw3/script.sh
@@ -0,0 +1,10 @@
+sudo find / -type -l
+
+sudo sed -n '/^Character/, /^$/ { /^$/ !p }' /proc/devices
+sudo sed -n '/^Block/, /^$/ { /^$/ !p }' /proc/devices
+
+sudo find . \! -perm /1000
+
+sudo ln -s /etc/hostname /tmp
+sudo useradd testuser
+sudo touch /home/testuser_data && sudo chown testuser:testuser /home/testuser_data
